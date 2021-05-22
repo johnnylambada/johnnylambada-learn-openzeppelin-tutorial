073a720c895639710bec26718504157bde2cd266
John Lombardo <john@lombardos.org>
2021-05-22 14:41:39 -0700

    Show the box value too

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,10 @@ module.exports = async function main(callback) {
     // Retrieve accounts from the local node
     const accounts = await web3.eth.getAccounts();
     console.log(accounts);
+    const Box = artifacts.require("Box");
+    const box = await Box.deployed();
+    const value = await box.retrieve();
+    console.log("Box value is", value.toString());
 
     // our code ends here
     callback(0);
