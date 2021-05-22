f298a56b377e037bda69f1623f00d88b405451a7
John Lombardo <john@lombardos.org>
2021-05-22 07:48:40 -0700

    npx eslint scripts --fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
 // scripts/index.js
 module.exports = async function main(callback) {
-    try {
-      // Our code will go here
+  try {
+    // Our code will go here
 
-      // Retrieve accounts from the local node
-      const accounts = await web3.eth.getAccounts();
-      console.log(accounts)
+    // Retrieve accounts from the local node
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
 
-      // our code ends here
-      callback(0);
-    } catch (error) {
-      console.error(error);
-      callback(1);
-    }
-}
+    // our code ends here
+    callback(0);
+  } catch (error) {
+    console.error(error);
+    callback(1);
+  }
+};
