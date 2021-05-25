7768d8181716896153511752b62556d4950a1832
John Lombardo <john@lombardos.org>
2021-05-24 17:30:34 -0700

    Fix more lint errors

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,5 +1,5 @@
 // migrations/2_deploy.js
-const Box = artifacts.require("Box");
+const Box = artifacts.require('Box');
 
 module.exports = async function (deployer) {
   await deployer.deploy(Box);
--- scripts/index.js
+++ scripts/index.js
@@ -6,17 +6,17 @@ module.exports = async function main(callback) {
     // Retrieve accounts from the local node
     const accounts = await web3.eth.getAccounts();
     console.log(accounts);
-    const Box = artifacts.require("Box");
+    const Box = artifacts.require('Box');
     const box = await Box.deployed();
     const value = await box.retrieve();
-    console.log("Box value is", value.toString());
+    console.log('Box value is', value.toString());
 
     // Send a transaction to store() a new value in the Box
     await box.store(23);
 
     // Call the retrieve() function of the deployed Box contract
-    newValue = await box.retrieve();
-    console.log("Box value is", newValue.toString());
+    const newValue = await box.retrieve();
+    console.log('Box value is', newValue.toString());
 
     // our code ends here
     callback(0);
