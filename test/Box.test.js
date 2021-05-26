43b91d6cb66d225a3de3219f811852abd4ee5a9b
John Lombardo <john@lombardos.org>
2021-05-25 18:54:12 -0700

    Now upgrade Box.test.js to use new test environment. Need to redo the test-helpers changes

--- test/Box.test.js
+++ test/Box.test.js
@@ -1,4 +1,34 @@
 // test/Box.test.js
+
+// Load dependencies
+const { accounts, contract } = require('@openzeppelin/test-environment');
+const { expect } = require('chai');
+
+// Load compiled artifacts
+const Box = contract.fromArtifact('Box');
+
+// Start test block
+describe('Box', () => {
+  const [owner] = accounts;
+
+  beforeEach(async function () {
+    // Deploy a new Box contract for each test
+    this.contract = await Box.new({ from: owner });
+  });
+
+  // Test case
+  it('retrieve returns a value previously stored', async function () {
+    // Store a value - recall that only the owner account can do this!
+    await this.contract.store(42, { from: owner });
+
+    // Test if the returned value is the same one
+    // Note that we need to use strings to compare the 256 bit integers
+    expect((await this.contract.retrieve()).toString()).to.equal('42');
+  });
+});
+
+// TODO: Add these more complex tests back now that we're using the openzeppelin test env
+/*
 // Load dependencies
 const { expect } = require('chai');
 
@@ -39,3 +69,4 @@ contract('Box', ([owner, other]) => {
     );
   });
 });
+*/
