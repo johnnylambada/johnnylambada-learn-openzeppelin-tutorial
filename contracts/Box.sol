5c663bb6ef00b28fc5f29666190109bf2d29d359
John Lombardo <john@lombardos.org>
2021-05-28 09:30:31 -0700

    Modify Box.sol so that it's compatible with upgrading

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,17 +2,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Import Ownable from the OpenZeppelin Contracts library
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 // Make Box inherit from the Ownable contract
-contract Box is Ownable {
+contract Box {
     uint256 private value;
 
     event ValueChanged(uint256 newValue);
 
     // The onlyOwner modifier restricts who can call the store function
-    function store(uint256 newValue) public onlyOwner {
+    function store(uint256 newValue) public {
         value = newValue;
         emit ValueChanged(newValue);
     }
